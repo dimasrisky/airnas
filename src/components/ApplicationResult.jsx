967e7b4c12d20107853f0ae537dd1e8cdae32e0f
Dimz~ <112872939+dimasrisky@users.noreply.github.com>
2023-11-17 13:34:37 +0700

    fixing bug layout di application result (#2)
    fixing bug layout di bagian weather result

--- src/components/ApplicationResult.jsx
+++ src/components/ApplicationResult.jsx
@@ -17,12 +17,12 @@ const ApplicationResult = ({ dataWeather }) => {
   const weatherIcon = (icon) => icon.includes("n") ? `/assets/icons/moon.png` : `/assets/icons/sun.png`;
 
   return (
-    <div className="flex items-center justify-center w-[100vw] h-[100vh]">
-      <nav className="absolute top-0 flex h-[60px] w-full items-center justify-between px-[4%] sm:h-[100px] sm:px-[10%]">
+    <div className="w-screen h-screen">
+      <nav className="flex h-[60px] w-full mx-auto items-center justify-between px-[4%] sm:h-[100px] sm:px-[10%]">
         <Link to="/weather">
           <div className="flex items-center gap-[10px]">
             <img src="/assets/icons/logo.png" alt="logo" className="h-[30px] w-[30px] sm:h-[50px] sm:w-[50px] "/>
-            <h1 className="text-[16px] font-bold sm:text-[24px]">Airnas</h1>
+            <h1 className="text-[16px] font-bold sm:text-[24px] sm:text-red-600">Airnas</h1>
           </div>
         </Link>
         <Link to="/weather-home">
@@ -30,28 +30,30 @@ const ApplicationResult = ({ dataWeather }) => {
         </Link>
       </nav>
 
-      <section className="flex justify-center items-center px-[4%] sm:px-[10%]">
-        <div className="weather-bg h-[498px] w-full max-w-[915px] rounded-[30px] sm:h-[683px] lg:h-[480px]">
-          <div className="flex h-full w-full flex-col items-center gap-[10px] py-[30px] lg:flex-row lg:justify-center lg:gap-[100px] px-[25px] sm:px-[100px]">
-            <div className="flex flex-col justify-center text-center">
-              <h1 className="textShadow text-[16px] font-bold text-white sm:text-[24px]">{city || "Loading"}</h1>
-              <h3 className="textShadow mb-[5px] text-[12px] font-bold text-white sm:text-[16px] ">{`${state || "Loading"}, ${country || "Loading"}`}</h3>
-              <p className="textShadow text-[12px] font-normal text-white sm:text-[16px] ">{date || "Loading"}</p>
-              <img src={weatherIcon(ic)} alt="icon" className="mx-auto -mt-[25px] h-[170px] w-[170px] sm:-mt-[10px] sm:h-[220px] sm:w-[220px]"/>
-              <h6 className="textShadow -mt-[20px] text-[28px] font-extrabold text-white sm:text-[38px] ">{`${tp}° C`}</h6>
-            </div>
-            <div className="flex flex-col justify-center gap-[20px]">
-              <h5 className="textShadow text-center text-[14px] font-bold text-white sm:text-[24px] lg:text-left">Informasi Cuaca Hari Ini</h5>
-              <div className="grid w-[100%] grid-cols-2 grid-rows-2 gap-[10px] sm:gap-[20px]">
+      <div className="weather-bg mx-auto rounded-[30px] w-[80%] lg:px-[2rem] lg:w-[70%] mt-[2rem]">
+        <div className="flex w-full mx-auto flex-col items-center py-[3rem] lg:py-[1.5rem] lg:flex-row lg:justify-center sm:gap-[1rem] lg:gap-[3rem]">
+          <div className="flex flex-col justify-between text-center">
+            <h1 className="textShadow text-[16px] font-bold text-white sm:text-[24px] ">{city || "Loading"}</h1>
+            <h3 className="textShadow mb-[5px] text-[12px] font-bold text-white sm:text-[16px] text-center">{`${state || "Loading"}, ${country || "Loading"}`}</h3>
+            <p className="textShadow text-[12px] font-normal text-white sm:text-[16px] ">{date || "Loading"}</p>
+            <img src={weatherIcon(ic)} alt="icon" className="mx-auto -mt-[25px] w-[170px] sm:w-[16rem]"/>
+            <h6 className="textShadow -mt-[20px] text-[28px] font-extrabold text-white sm:text-[38px] ">{`${tp}° C`}</h6>
+          </div>
+          <div className="flex flex-col justify-center gap-[20px]">
+            <h5 className="textShadow text-center text-[14px] font-bold text-white sm:text-[24px] lg:text-left">Informasi Cuaca Hari Ini</h5>
+            <div className="flex flex-col gap-[10px] sm:gap-[3rem]">
+              <div className="flex gap-[1rem]">
                 <InformationsCard icon="/assets/icons/winds.png" title="Wind Speed" data={`${ws}m/s`}/>
                 <InformationsCard icon="/assets/icons/airquality.png" title="Air Quality US" data={aqius}/>
+              </div>
+              <div className="flex gap-[1rem]">
                 <InformationsCard icon="/assets/icons/pressure.png" title="Pressure" data={`${pr} hpa`}/>
                 <InformationsCard icon="/assets/icons/humidity.png" title="Humidity" data={`${hu}%`}/>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
