d3be44ddd27fe9f7f08493d1097d9df18712af23
Dimz~ <112872939+dimasrisky@users.noreply.github.com>
2023-11-25 19:49:35 +0700

    Bugfix/landing page layout issue (#7)
    * style(landingPage): reformating kode html (prettier susah dibaca njir)
    
    * fix(landingPage): fix layout yang berantakan di bagian landing page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,13 @@
 const Article = ({ img, title, date, articleLink }) => {
   return (
     <>
-      <div className="flex justify-between gap-8 xl:flex-row-reverse">
-        <img src={`/assets/img/${img}`} alt="article" />
-        <div className="flex flex-col justify-between">
-          <a
-            href={articleLink}
-            className="text-primary-800 text-[0.6rem] font-semibold sm:text-[0.8rem] xl:text-[0.7rem]"
-          >
+      <div className="w-full xl:w-[565px] flex gap-8 xl:flex-row-reverse justify-start">
+        <img src={`/assets/img/${img}`} alt="article" className="w-[89px] sm:w-[130px] " />
+        <div className="flex flex-col gap-4 justify-between">
+          <a href={articleLink} target="_blank" className="text-primary-800 text-[14px] font-semibold sm:text-[20px]">
             {title}
           </a>
-          <p className="text-[0.6rem] font-semibold text-[#A54230]">{date}</p>
+          <p className="text-[12px] sm:text-[16px] font-semibold text-[#A54230]">{date}</p>
         </div>
       </div>
     </>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,50 +14,50 @@ const AboutPage = ({ dataWeather }) => {
   return (
     <>
       {/* Navbar */}
-        <nav className='fixed top-0 z-50'>
-            <div className='w-[100vw] border-b-2 py-[0.6rem] shadow-md bg-white'>
-                <div className='w-[85%] mx-auto flex justify-between items-center'>
-                    <div className='flex items-center gap-1'>
-                        <img src="/assets/icons/logo.png" alt="logo" className='scale-75'/>
-                        <h3 className='font-bold text-lg'>Airnas</h3>
-                    </div>
-                    <div className='hidden xl:flex items-center gap-3'>
-                        <img src="/assets/icons/location.png" alt="location" />
-                        <div>
-                            <p className='text-[0.7rem]'>Lokasi</p>
-                            <p className='text-[0.7rem] text-primary-500 font-semibold'>{`${dataWeather?.country}, ${dataWeather?.city}`}</p>
-                        </div>
-                        <Link to='/weather-home' className='bg-primary-500 text-white rounded-md px-4 py-2 text-[0.7rem] ml-[1.5rem] btn-shadow'>Coba Airnas</Link>
-                    </div>
-
-                    {/* nav-link For Mobile Mode */}
-                    <div className='xl:hidden relative'>
-                        <img src={`/assets/svg/${isNavOpen? 'x.svg' : 'burger-nav.svg'}`} alt="nav-icon" className='scale-125' onClick={() => setIsNavOpen(prev => !prev)} />
-                        {isNavOpen? (
-                            <div className='bg-white w-[7rem] text-[0.7rem] text-center flex flex-col shadow-lg px-2 py-4 rounded-[0.5rem] gap-3 absolute top-2 right-4'>
-                                <Link to="/landing-page" className='hover:text-primary-500 transition-all duration-200'>Home</Link>
-                                <a href="#penyebab" className='hover:text-primary-500 transition-all duration-200'>Penyebab</a>
-                                <a href="#statistik" className='hover:text-primary-500 transition-all duration-200'>Statistik</a>
-                                <a href="#ringkasan" className='hover:text-primary-500 transition-all duration-200'>Ringkasan</a>
-                                <a href="#berita" className='hover:text-primary-500 transition-all duration-200'>Berita</a>
-                                <Link to='/weather-home' className='bg-primary-500 text-white py-2 px-2 text-[0.6rem] rounded-md'>Coba Airnas</Link>
-                            </div>
-                        ) : ''}
-                    </div>
-                </div>
+      <nav className="fixed top-0 z-50">
+        <div className="w-[100vw] border-b-2 bg-white py-[1rem] shadow-md">
+          <div className="mx-auto flex w-[85%] items-center justify-between">
+            <div className="flex items-center gap-2">
+              <img src="/assets/icons/logo.png" alt="logo" className="w-[30px] sm:w-[50px]"/>
+              <h3 className="text-[16px] sm:text-[24px] font-bold">Airnas</h3>
+            </div>
+            <div className="hidden items-center gap-3 xl:flex">
+              <img src="/assets/icons/location.png" alt="location" className="w-[48px]"/>
+              <div>
+                <p className="text-[12px]">Lokasi</p>
+                <p className="text-primary-500 text-[16px] font-semibold">{`${dataWeather?.country}, ${dataWeather?.city}`}</p>
+              </div>
+              <Link to="/weather-home" className="bg-primary-500 btn-shadow ml-[1.5rem] rounded-md px-4 py-2 text-[18px] w-[151px] text-center text-white">Coba Airnas</Link>
             </div>
 
-            {/* nav-link desktop mode */}
-            <div className='hidden xl:flex bg-white w-[100vw] border-b-2 py-[1rem] shadow-md'>
-                <div className='w-[85%] mx-auto flex gap-[2rem] text-[0.8rem]'>
-                    <Link to="/landing-page" className='hover:text-primary-500 transition-all duration-200'>Home</Link>
-                    <a href="#penyebab" className='hover:text-primary-500 transition-all duration-200'>Penyebab</a>
-                    <a href="#statistik" className='hover:text-primary-500 transition-all duration-200'>Statistik</a>
-                    <a href="#ringkasan" className='hover:text-primary-500 transition-all duration-200'>Ringkasan</a>
-                    <a href="#berita" className='hover:text-primary-500 transition-all duration-200'>Berita</a>
+            {/* nav-link For Mobile Mode */}
+            <div className="relative xl:hidden">
+              <img src={`/assets/svg/${isNavOpen ? "x.svg" : "burger-nav.svg"}`} alt="nav-icon" className="w-[30px]" onClick={() => setIsNavOpen(prev => !prev)}/>
+              {isNavOpen ? (
+                <div className="absolute right-4 top-2 flex w-[7rem] flex-col gap-3 rounded-[0.5rem] bg-white px-2 py-4 text-center text-[0.7rem] shadow-lg">
+                  <a href="#" className="hover:text-primary-500 transition-all duration-200" >Home</a>
+                  <a href="#penyebab" className="hover:text-primary-500 transition-all duration-200">Penyebab</a>
+                  <a href="#statistik" className="hover:text-primary-500 transition-all duration-200">Statistik</a>
+                  <a href="#ringkasan" className="hover:text-primary-500 transition-all duration-200">Ringkasan</a>
+                  <a href="#berita" className="hover:text-primary-500 transition-all duration-200">Berita</a>
+                  <Link to="/weather-home" className="bg-primary-500 rounded-md px-2 py-2 text-[0.6rem] text-white">Coba Airnas</Link>
                 </div>
+              ) : null}
             </div>
-        </nav>
+          </div>
+        </div>
+
+        {/* nav-link desktop mode */}
+        <div className="hidden w-[100vw] border-b-2 bg-white py-[1rem] shadow-md xl:flex">
+          <div className="mx-auto flex w-[85%] gap-[2rem] text-[1rem]">
+            <a href="#" className="hover:text-primary-500 transition-all duration-200">Home</a>
+            <a href="#penyebab" className="hover:text-primary-500 transition-all duration-200">Penyebab</a>
+            <a href="#statistik" className="hover:text-primary-500 transition-all duration-200">Statistik</a>
+            <a href="#ringkasan" className="hover:text-primary-500 transition-all duration-200">Ringkasan</a>
+            <a href="#berita" className="hover:text-primary-500 transition-all duration-200">Berita</a>
+          </div>
+        </div>
+      </nav>
 
         {/* About Section */}
         <main className='w-[85%] xl:w-[70%] mx-auto mt-[6rem] xl:mt-[10rem]'>
@@ -83,80 +83,86 @@ const AboutPage = ({ dataWeather }) => {
             </div>
         </main>
 
-        {/* Footer */}
-        <footer className='w-[100vw] bg-primary-500 mt-[6rem] text-white rounded-t-[2rem]'>
-            <div className='w-[85%] xl:flex xl:flex-row xl:justify-between xl:items-baseline xl:gap-[3rem] mx-auto py-7'>
-                <div className='flex flex-col gap-1'>
-                    <img src="/assets/icons/logo.png" alt="logo" className='w-[2rem]'/>
-                    <h1 className='font-bold'>Airnas</h1>
-                    <p className='font-medium text-[0.6rem] xl:text-[0.7rem] opacity-70 max-w-[80%]'>Airnas adalah aplikasi web yang berfungsi untuk mengecek cuaca saat ini di daerah pengguna. Aplikasi tersedia secara gratis untuk semua pengguna. Aplikasi ini dapat diakses melalui browser web di komputer, laptop, tablet, dan smartphone.</p>
+       {/* Footer */}
+      <footer className="bg-primary-500 mt-[7rem] w-[100vw] rounded-t-[2rem] text-white">
+        <div className="mx-auto w-[85%] py-7 xl:flex xl:flex-row xl:items-baseline xl:justify-between xl:gap-[3rem]">
+          <div className="flex flex-col gap-1">
+            <img src="/assets/icons/logo.png" alt="logo" className="w-[30px] sm:w-[50px]" />
+            <h1 className="font-bold text-[16px] sm:text-[24px]">Airnas</h1>
+            <p className="max-w-[80%] text-[12px] font-medium opacity-70 sm:text-[14px]">
+              Airnas adalah aplikasi web yang berfungsi untuk mengecek cuaca
+              saat ini di daerah pengguna. Aplikasi tersedia secara gratis untuk
+              semua pengguna. Aplikasi ini dapat diakses melalui browser web di
+              komputer, laptop, tablet, dan smartphone.
+            </p>
+          </div>
+          {/* Dropdown */}
+          <div className="mt-[3rem] flex w-full flex-col gap-[2.5rem] sm:flex-row">
+            {/* Perusahaan */}
+            <div>
+              <div className="flex items-center justify-between" onClick={() => setIsPerusahaanDropdownOpen((prev) => !prev)}>
+                <h1 className="text-[14px] sm:text-[16px] font-bold uppercase">perusahaan</h1>
+                <img src="/assets/icons/arrow.png" alt="arrow" className={`${isPerusahaanDropdownOpen ? "rotate-0" : "rotate-[180deg]"} sm:hidden`}/>
+              </div>
+              <div className="mt-[1rem] hidden flex-col gap-2 text-[14px] font-semibold sm:flex">
+                <Link to="/about-page">Tentang Kami</Link>
+                <a href="">Kebijakan Privasi</a>
+                <a href="">Syarat dan ketentuan</a>
+                <a href="">Bantuan</a>
+              </div>
+
+              {/* Dropdown Mobile */}
+              {isPerusahaanDropdownOpen ? (
+                <div className="mt-[1rem] flex flex-col gap-2 text-[14px] font-semibold sm:hidden">
+                  <Link to="/about-page">Tentang Kami</Link>
+                  <a href="">Kebijakan Privasi</a>
+                  <a href="">Syarat dan ketentuan</a>
+                  <a href="">Bantuan</a>
                 </div>
-                {/* Dropdown */}
-                <div className='w-full flex flex-col sm:flex-row gap-[3rem] mt-[3rem]'>
-                    {/* Perusahaan */}
-                    <div>
-                        <div className='flex justify-between items-center' onClick={() => setIsPerusahaanDropdownOpen(prev => !prev)}>
-                            <h1 className='uppercase font-bold text-[0.8rem]'>perusahaan</h1>
-                            <img src="/assets/icons/arrow.png" alt="arrow" className={`${isPerusahaanDropdownOpen? 'rotate-0' : 'rotate-[180deg]'} sm:hidden`} />
-                        </div>
-                        <div className='hidden sm:flex flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                            <a href="">Tentang Kami</a>
-                            <a href="">Kebijakan Privasi</a>
-                            <a href="">Syarat dan ketentuan</a>
-                            <a href="">Bantuan</a>
-                        </div>
-                         {/* Dropdown Mobile */}
-                        {isPerusahaanDropdownOpen? (
-                            <div className='flex sm:hidden flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                                <Link to='/about-page' href="">Tentang Kami</Link>
-                                <a href="">Kebijakan Privasi</a>
-                                <a href="">Syarat dan ketentuan</a>
-                                <a href="">Bantuan</a>
-                            </div>
-                        ) : ''}
-                    </div>
-                    {/* Sosial Media */}
-                    <div>
-                        <div className='flex justify-between items-center' onClick={() => setIsSosialMediaOpen(prev => !prev)}>
-                            <h1 className='uppercase font-bold text-[0.8rem]'>Sosial Media</h1>
-                            <img src="/assets/icons/arrow.png" alt="arrow" className={`${isSosialMediaOpen? 'rotate-0' : 'rotate-[180deg]'} sm:hidden`} />
-                        </div>
-                        <div className='hidden sm:flex flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                            <a href="">Twitter</a>
-                            <a href="">Facebook</a>
-                            <a href="">Instagram</a>
-                        </div>
-                         {/* Dropdown Mobile */}
-                        {isSosialMediaOpen? (
-                            <div className='flex flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                                <a href="">Twitter</a>
-                                <a href="">Facebook</a>
-                                <a href="">Instagram</a>
-                            </div>
-                        ) : ''}
-                    </div>
-                    {/* Lainnya */}
-                    <div>
-                        <div className='flex justify-between items-center' onClick={() => setIsLainnyaOpen(prev => !prev)}>
-                            <h1 className='uppercase font-bold text-[0.8rem]'>lainnya</h1>
-                            <img src="/assets/icons/arrow.png" alt="arrow" className={`${isLainnyaOpen? 'rotate-0' : 'rotate-[180deg]'} sm:hidden`} />
-                        </div>
-                        <div className='hidden sm:flex flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                            <a href="">Lisensi</a>
-                            <a href="">Partnership</a>
-                        </div>
-                         {/* Dropdown Mobile */}
-                        {isLainnyaOpen? (
-                            <div className='flex flex-col font-semibold text-[0.6rem] gap-2 mt-[1rem]'>
-                                <a href="">Lisensi</a>
-                                <a href="">Partnership</a>
-                            </div>
-                        ) : ''}
-                    </div>
-                </div>
+              ) : null}
             </div>
-            <div className='bg-primary-500 border-t border-primary-700 text-center text-[0.6rem] py-3'>COPYRIGHT © 2023 — Airnas | Asta Arkananta <br /> SMKN 8 MALANG</div>
-        </footer>
+            {/* Sosial Media */}
+            <div>
+              <div className="flex items-center justify-between" onClick={() => setIsSosialMediaOpen((prev) => !prev)}>
+                <h1 className="text-[14px] sm:text-[16px] font-bold uppercase">Sosial Media</h1>
+                <img src="/assets/icons/arrow.png" alt="arrow" className={`${ isSosialMediaOpen ? "rotate-0" : "rotate-[180deg]" } sm:hidden`}/>
+              </div>
+              <div className="mt-[1rem] hidden flex-col gap-2 text-[14px] font-semibold sm:flex">
+                <a href="">Twitter</a>
+                <a href="">Facebook</a>
+                <a href="">Instagram</a>
+              </div>
+              {/* Dropdown Mobile */}
+              {isSosialMediaOpen ? (
+                <div className="mt-[1rem] flex flex-col gap-2 text-[14px] font-semibold">
+                  <a href="">Twitter</a>
+                  <a href="">Facebook</a>
+                  <a href="">Instagram</a>
+                </div>
+              ) : null}
+            </div>
+            {/* Lainnya */}
+            <div>
+              <div className="flex items-center justify-between" onClick={() => setIsLainnyaOpen((prev) => !prev)}>
+                <h1 className="text-[14px] sm:text-[16px] font-bold uppercase">lainnya</h1>
+                <img src="/assets/icons/arrow.png" alt="arrow" className={`${ isLainnyaOpen ? "rotate-0" : "rotate-[180deg]"} sm:hidden`}/>
+              </div>
+              <div className="mt-[1rem] hidden flex-col gap-2 text-[14px] font-semibold sm:flex">
+                <a href="">Lisensi</a>
+                <a href="">Partnership</a>
+              </div>
+              {/* Dropdown Mobile */}
+              {isLainnyaOpen ? (
+                <div className="mt-[1rem] flex flex-col gap-2 text-[14px] font-semibold">
+                  <a href="">Lisensi</a>
+                  <a href="">Partnership</a>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <div className="bg-primary-500 border-primary-700 border-t py-3 text-center text-[12px] sm:text-[14px] tracking-[0.2rem]">COPYRIGHT © 2023 — Airnas | Asta Arkananta <span className="block sm:inline-block">SMKN 8 MALANG</span></div>
+      </footer>
     </>
   )
 }
