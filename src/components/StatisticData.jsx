97a40ba3e3d90fe4d7aa503ef662e510a1fcc7ac
Dimz~ <rizkyalphazee@gmail.com>
2023-11-25 19:59:52 +0700

    fix(landingPage): fix layout yang berantakan di bagian landing page

--- src/components/StatisticData.jsx
+++ src/components/StatisticData.jsx
@@ -4,8 +4,8 @@ const StatisticData = ({ text1, text2, value}) => {
   return (
     <>
     <div>
-        <h4 className='font-semibold text-primary-800 text-[0.8rem]'>{text1}<br />{text2}</h4>
-        <h1 className='font-extrabold text-primary-700 text-[1.5rem]'>{value}</h1>
+        <h4 className='font-semibold text-primary-800 text-[16px] sm:text-[18px]'>{text1}<br />{text2}</h4>
+        <h1 className='font-extrabold text-primary-700 text-[32px] sm:text-[44px]'>{value}</h1>
     </div>
     </>
   )
